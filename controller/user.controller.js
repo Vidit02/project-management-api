9643e1af0f12afe42967041243d2731eb23ca291
“Vidit <vidit.21102@gmail.com>
2023-08-02 01:00:41 +0530

    Passport js and login with bearer token done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,33 +1,129 @@
+const validator = require("validator")
 const express = require("express")
 const userModel = require("../models/users.model")
+const bcrypt = require("bcrypt")
 
-function login(req, res) {
+const jwt = require('jsonwebtoken')
+const tokenSecret = "my-token-secret"
+function signup(req,res){
+    let name = req.body.name
     let emailid = req.body.emailid
     let password = req.body.password
-    console.log("emaild :" + emailid + "password " + password);
-    if (emailid == "" || password == "") {
+    let role = req.body.role
+
+    let errors = {}
+    let isError = false 
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    let passregex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/
+
+    if (name == undefined || validator.isEmpty(name)) {
+        isError = true
+        errors.nameError = "Please Enter Name"
+    }
+    if (emailid == undefined || !validator.isEmail(emailid)) {
+        isError = true
+        errors.emailIdError = "Please Enter Email in correct Format"
+    }
+    if (password == undefined || !passregex.test(password)) {
+        isError = true
+        errors.passwordError = "Should Contain 1 Capital,1 Small,1 Number & 1 Special Char"
+    }
+
+    if(isError){
         res.json({
-            status: 402,
-            msg: "Something is wrong",
-            data: "dfailuew"
+            status : 401,
+            msg : "Correct all errors",
+            data : req.body,
+            errors : errors
         })
     } else {
-        userModel.findOne({ "emailid": emailid }).exec(function (err, success) {
-            if (err) {
+        userModel.findOne({"emailId":emailid},(err,success)=>{
+            if(success){
                 res.json({
-                    status: 402,
-                    msg: "Something is wrong",
-                    data: req.body
+                    status : 402,
+                    data : emailid,
+                    msg : "Email Already Exists"
                 })
             } else {
-                res.json({
-                    status: 402,
-                    msg: "Something is wrong",
-                    data: success
+                let user = new userModel({
+                    "name" : name,
+                    "emailId" : emailid,
+                    "password" : hash,
+                    "role" : role
+                })
+
+                user.save(function (err,success){
+                    if(err){
+                        res.json({
+                            status : 402,
+                            msg : "Something is wrong",
+                            data : req.body
+                        })
+                    } else {
+                        res.json({
+                            status: 200,
+                            msg : "Signup is done",
+                            data : success
+                        })
+                    }
                 })
             }
-
         })
     }
 }
+
+function login(req,res){
+    let emailid = req.body.emailid
+    let password = req.body.password
+    if(emailid == "" || password == ""){
+        res.json({
+            status : 401,
+            msg : "Something is wrong",
+            data : req.body
+        })
+    } else {
+        userModel.findOne({"emailId":emailid}).populate("role").exec(function(err,success){
+            if(err || err == null && success == null) {
+                res.json({
+                    status : 402,
+                    msg : "Something is wrong",
+                    data : req.body
+                })
+            } else {
+                bcrypt.compare(password , success.password , function(err,result){
+                    if(err){
+                        res.json({
+                            status : 404,
+                            msg : "Something is wrong",
+                            data : req.body
+                        })
+                    }if(result){
+                        const payload = {
+                            id : success._id,
+                            name : success.name,
+                            emailId : success.emailId,
+                            role : success.role
+                        }
+                        const token = jwt.sign(payload,tokenSecret)
+                        res.json({
+                            status : 200,
+                            msg : "User Found",
+                            token : "Bearer "+token,
+                            role : success.role
+                        })
+                    } else {
+                        res.json({
+                            status : 401,
+                            msg : "Invalid Credentials",
+                            data : "Please try again"
+                        })
+                    }
+                })
+            }
+        })
+    }
+}
+
+module.exports.signup = signup
 module.exports.login = login
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,5 +2,6 @@ var router = require("express").Router()
 const userController = require("../controller/user.controller")
 
 router.post("/login",userController.login)
+router.post("/signup",userController.signup)
 
 module.exports = router
